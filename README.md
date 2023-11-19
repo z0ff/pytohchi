2943b9a0cc1f5457e0b898cba3bde8419a15f60c
z0ff <ex1999@msn.com>
2023-11-20 03:13:17 +0900

    述語を先頭に移動するinvert_predicate関数を追加

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyTohchi (Py倒置)
+PyTohchiは、日本語文を倒置法に変換するPythonライブラリです。
--- pytohchi/main.py
+++ pytohchi/main.py
@@ -2,11 +2,39 @@ import spacy
 
 nlp = spacy.load('ja_ginza')
 
-def hoge(s: str):
+
+# 述語を先頭に移動
+def invert_predicate(s: str):
+    root = None
     doc = nlp(s)
 
-    for sent in doc.sents:
-        for token in sent:
-            print(token.i, token.orth_, token.lemma_, token.pos_, 
-                token.tag_, token.dep_, token.head.i)
+    tokens = [[token for token in sent] for sent in doc.sents]
 
+    for sent_no in range(len(tokens)):
+        for token_no in range(len(tokens[sent_no])):
+            if tokens[sent_no][token_no].dep_ == 'ROOT':
+                root = tokens[sent_no][token_no].i
+                break
+        if root is not None:
+            predicate = tokens[sent_no][root:]
+            tokens[sent_no][root:] = []
+            tokens[sent_no][0:0] = predicate
+
+    for sent in tokens:
+        for token in sent:
+            if token.dep_ == 'ROOT':
+                print(token.orth_, end='')
+            elif token.i < root:
+                print(token.orth_, end='')
+            else:
+                print(token.orth_, end='')
+        print()
+
+
+if __name__ == '__main__':
+    while True:
+        print('文を入力。終了するときはexit=> ', end='')
+        s = input()
+        if s == 'exit':
+            break
+        invert_predicate(s)
